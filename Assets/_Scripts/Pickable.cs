0eacf951a7d74ae4f356c50f924511e8d56e5b77
Raúl Blas Ruiz <raulblas@ucm.es>
2019-06-07 14:57:51 +0200

    Añadido Pickable.cs

---
+++ Assets/_Scripts/Pickable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickable : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(other);
+        }
+    }
+}
